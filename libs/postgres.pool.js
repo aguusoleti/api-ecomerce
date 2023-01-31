1887b83892ee47e3c0ee4769015012859664e43a
aguusoleti <agustin_soleti@hotmail.com>
2023-01-31 13:33:26 -0300

    fallando

---
+++ libs/postgres.pool.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg')
+const{config}= require('../config/config')
+
+const USER = encodeURIComponent(config.dbUser)
+const PASSWORD = encodeURIComponent(config.dbPassword)
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+
+const pool = new Pool({connectionString: URI });
+
+     // const pool = new Pool({
+    //     host: 'localhost',
+    //     port: 5432,
+    //     user:'postgres',
+    //     password: 'aguusoleti',
+    //     database:'Lets_Print',
+    // });
+    
+   
+   
+module.exports=pool; 
